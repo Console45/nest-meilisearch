7383bc862cb9bb47e1d2d3eccb87b6e08e201124
Cosmos Appiah <cosmosappiah029@gmail.com>
2022-10-15 22:08:10 +0000

    made indices option optional

--- src/meilisearch.interface.ts
+++ src/meilisearch.interface.ts
@@ -3,6 +3,6 @@ import { Config, IndexOptions } from "meilisearch";
 export interface MeiliSearchOptions extends Config {
   indices?: {
     uid: string;
-    options: IndexOptions;
+    options?: IndexOptions;
   }[];
 }
